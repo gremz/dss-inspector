a68ccd8566fd27482774b8ae6448f4354310657e
gremiasz <gremiasz@tribune.com>
2014-01-21 01:17:33 -0600

    added last updated and busy

--- dss-inspector.js
+++ dss-inspector.js
@@ -16,6 +16,8 @@
                         'text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);' +
                         'border: 1px solid #fbeed5;' +
                         'border-radius: 4px;' +
+                        'background-color: #fcf8e3;' +
+                        'color: #c09853;' +
                       '}' +
                       '.error {' +
                         'background-color: #f2dede;' +
@@ -29,6 +31,9 @@
                       '}' +
                       '.status {' +
                         'white-space: pre-wrap;' +
+                      '}' +
+                      '.busy:after {' +
+                        'content: "Wating for metering (or it doesn\'t exist)...";' +
                       '}' +
                       '.dss_url_pattern_container.hidden .dss_inspector_url_pattern {' +
                         'display: none;' +
@@ -47,39 +52,46 @@
                       '}';
 
   $('head').append('<style>' + inspectorCss + '</style>');
-  $body.append('<pre class="status"></pre>');
+  $body.append('<h4>Metering Status</h4><pre class="status"></pre>');
+  $body.append('<div class="dss_inspector_activity"></div><span class="refresh_time"></span>');
 
   function checkMetering() {  
     // check for metering service
     var ms = window && window.opener && window.opener.trb && window.opener.trb.meteringService;
     if (!ms) {
-      // showAlert('Oh snap! Metering does not live on this page', 'error'); //alpha
-      setTimeout( function() {
-        checkMetering();
-      }, 2000);
+      // showAlert('Oh snap! Metering does not live on this page...or not yet', 'error'); //alpha
+      $('.dss_inspector_activity').addClass('alert busy');
+      keepChecking(2000);
       return;
     }
-
-    // remove errors
-    // $('.error').remove(); //alpha
 
     ms.getStatus( function(data) {
       // showAlert('Metering status returned successfully!', 'success'); //alpha
       var formattedData = JSON.stringify(data, null, '\t');
       formattedData = formattedData.replace(/urlPattern\"\:(\s\".+\")/gi, 'urlPattern": <span class="dss_url_pattern_container hidden"><span class="dss_url_pattern_visibility"></span><span class="dss_inspector_url_pattern" >$1</span></span>');// match pattern
       formattedData = formattedData.replace(/\"(.+\"\:)/gi, '<span class="dss_inspector_title">$1').replace(/\"(\:)/gi, ':</span>'); // match titles
-      updateStatus('<h3>Metering Status</h3>' + formattedData  );
+      updateStatus(formattedData);
+      $('.refresh_time').html('Last Successful update: ' + (new Date).toString());
 
       $('.dss_url_pattern_visibility').click( function(e) {
         $(this).parent().toggleClass('hidden');
       });
+
+      $('.dss_inspector_activity').removeClass('alert busy');
     });
-    setTimeout( function() {
-      checkMetering();
-    }, 4000);
+
+    keepChecking(4000);
   };
   checkMetering();
 
+  function keepChecking(time, callback) {
+    if (!time) {
+      return;
+    }
+    setTimeout( function() {
+      checkMetering();
+    }, time);
+  }
   function showAlert(msg, type) {
     $body.append('<div class="alert ' + (type ? type : '') + '">' + msg + '</div>');
   }
